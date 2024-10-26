c93229464e9c062dfab32d6f68b32e79f9a46d5b
eziraa <ezratgab@gmail.com>
2024-10-26 18:39:12 +0300

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,45 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	theme: {
+		
+		container: {
+			center: true,
+			padding: '15px',
+		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '960px',
+			xl: '1280px'
+		},
+		fontFamily: {
+			primary: 'var(--font-jetbrainsMono)',
+		},
+		extend: {
+			colors: {
+				primary: '#1c1c122',
+				accent: {
+					DEFAULT: '#00FF99',
+					hover: '#00E187'
+				}
+
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			
+		},
+		plugins: [tailwindcssAnimate],
+	}
 };
 export default config;
