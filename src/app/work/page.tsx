71b70628e800d550fb5f8e98072275c875ac62a3
eziraa <ezratgab@gmail.com>
2024-10-27 18:16:48 +0300

    fix interface issue

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -148,8 +148,11 @@ const Work = () => {
                   </div>
                 </SwiperSlide>
               ))}
-              <WorkSlideBtns containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none "
-              btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all duration-500 "/>
+              <WorkSlideBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none "
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all duration-500 "
+                iconsStyles=""
+              />
             </Swiper>
           </div>
         </div>
