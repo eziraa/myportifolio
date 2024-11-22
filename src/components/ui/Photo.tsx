29506468c03979695e0b9114bced0a3f539fe3d3
eziraa <ezratgab@gmail.com>
2024-11-23 00:13:48 +0300

    finish project

--- src/components/ui/Photo.tsx
+++ src/components/ui/Photo.tsx
@@ -40,7 +40,35 @@ const Photo = () => {
             className="object-contain rounded-full"
           />
         </motion.div>
-
+        <motion.svg
+          className="pt-2 w-[280px] h-[280px] xl:w-[320px] xl:h-[320px] "
+          fill="transparent"
+          viewBox="0 0 700 700"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx={350}
+            cy={353}
+            r={350}
+            className="mt-4"
+            stroke="transparent"
+            strokeWidth={4}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{
+              strokeDasharray: "24 10 0 0",
+            }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 92", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
