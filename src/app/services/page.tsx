aef292248715391bd7db77da9c55641aca88a09d
eziraa <ezratgab@gmail.com>
2024-11-23 00:11:39 +0300

    finish project

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -32,7 +32,7 @@ const Services = () => {
               ease: "easeIn",
             },
           }}
-          className="grid grid-cols-1 lg:grid-cols-3 gap-[60px]"
+          className="grid grid-cols-1 lg:grid-cols-2 gap-[60px]"
         >
           {services.map(({ num, title, href }, index) => (
             <div
